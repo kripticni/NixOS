12f035633db38ac907bc5b76a012637a457f2a1f
kripticni <kripticno@gmail.com>
2024-10-03 21:08:40 +0200

    added sddm fully themed, background for dwm, other additions + garbage collecting. Forgot to mention stylix support in the pervious commit.

---
+++ assets/scripts/autostart.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+#picom --no-use-damage --backend glx --vsync -b &>/dev/null &
+#polybar left &>/dev/null &
+#polybar right &>/dev/null &
+#polybar middle &>/dev/null &
+#polybar tray &>/dev/null &
+#polybar xwindow &>/dev/null &
+feh --bg-fill /home/aleksic/NixOS/assets/backgrounds/mountains.png
