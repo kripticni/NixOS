5b7f6d94a29bd71768c1d5b7c56ec4ff60770043
kripticni <kripticno@gmail.com>
2024-12-25 21:50:04 +0100

    configuring

--- sys/dwm/dwm/config.h
+++ sys/dwm/dwm/config.h
@@ -3,8 +3,8 @@
 /* appearance */
 static const unsigned int borderpx = 3; /* border pixel of windows */
 static const unsigned int snap = 4;     /* snap pixel */
-static const unsigned int gappih = 8;   /* horiz inner gap between windows */
-static const unsigned int gappiv = 8;   /* vert inner gap between windows */
+static const unsigned int gappih = 6;   /* horiz inner gap between windows */
+static const unsigned int gappiv = 6;   /* vert inner gap between windows */
 static const unsigned int gappoh =
     6; /* horiz outer gap between windows and screen edge */
 static const unsigned int gappov =
@@ -15,10 +15,10 @@ static const int swallowfloating =
     0;                        /* 1 means swallow floating windows by default */
 static const int showbar = 1; /* 0 means no bar */
 static const int topbar = 1;  /* 0 means bottom bar */
-static const int horizpadbar = -8; /* horizontal padding for statusbar */
+static const int horizpadbar = -6; /* horizontal padding for statusbar */
 static const int vertpadbar = 0;   /* vertical padding for statusbar */
-static const int vertpad = 8;      /* vertical padding of bar */
-static const int sidepad = 8;      /* horizontal padding of bar */
+static const int vertpad = 6;      /* vertical padding of bar */
+static const int sidepad = 6;      /* horizontal padding of bar */
 static const char *fonts[] = {
     "Iosevka:style:medium:size=12",
     "JetBrainsMono Nerd Font Mono:style:medium:size=19",
@@ -147,7 +147,7 @@ static const Key keys[] = {
     {MODKEY, XK_j, focusstack, {.i = +1}},
     {MODKEY, XK_k, focusstack, {.i = -1}},
     {MODKEY, XK_i, incnmaster, {.i = +1}},
-    {MODKEY, XK_u, incnmaster, {.i = -1}},
+    {MODKEY, XK_o, incnmaster, {.i = -1}},
     {MODKEY, XK_h, setmfact, {.f = -0.05}},
     {MODKEY, XK_l, setmfact, {.f = +0.05}},
     {MODKEY | ShiftMask, XK_k, setcfact, {.f = +0.25}},
@@ -159,7 +159,6 @@ static const Key keys[] = {
     {MODKEY, XK_Tab, view, {0}},
     {MODKEY, XK_q, killclient, {0}},
     {MODKEY, XK_t, setlayout, {.v = &layouts[0]}},
-    {MODKEY, XK_f, setlayout, {.v = &layouts[1]}},
     {MODKEY, XK_m, setlayout, {.v = &layouts[2]}},
     //{MODKEY, XK_space, setlayout, {0}},
     {MODKEY | ShiftMask, XK_space, togglefloating, {0}},
